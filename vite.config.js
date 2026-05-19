9e6e523a5406ed8b2939ea3bb7369f14353bd290
ankitsaini85 <nareshsaini85879@gmail.com>
2026-05-19 18:23:37 +0530

    frontend setup

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+});
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,          // allow external access
+//     port: 5173,
+//     allowedHosts: [
+//       'foresighted-michale-predorsal.ngrok-free.dev'
+//     ]
+//   }
+// });
